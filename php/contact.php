88a94beb99dcde59889282c68339b0fbf8d66ce5
Wulfsbach <etemelkos@gmail.com>
2018-09-13 18:58:24 -0400

    edited some stuff

--- php/contact.php
+++ php/contact.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "yourmail@gmail.com";
+        $recipient = "etemelkos@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
